f56bc5ac6ef4fd6531d0c3ba7f0d1ea1eaab5902
StephenZhu <zhuzp2@lianchuang.com>
2014-12-25 14:43:07 +0800

    1

---
+++ data/config.js
@@ -0,0 +1,26 @@
+exports.from = 'qiaoliang<qiaolianggame@163.com>';
+exports.config = {
+    user: 'qiaolianggame',
+    password: 'heroes2009',
+    host: 'smtp.163.com',
+    ssl: true
+};
+
+exports.config1 = {
+    host: 'smtp.163.com',
+    auth: {
+        user: 'qiaolianggame',
+        pass: 'heroes2009'
+    },
+    secure:'ssl'
+};
+exports.senddate = '2014年12月25日00:46:29111';
+
+exports.name = '<$name$>';
+exports.usrid = '<$yuangongid$>';
+exports.departname = '<$departname$>';
+exports.sendtime = '<$sendtime$>';
+exports.zuizhongshifa = '<$zuizhongshifa$>';
+
+
+
